524df1d5f154def486d4f74e1605db79b9bfa0ec
Piotr Dobiech <pitcer@protonmail.com>
2020-02-02 22:03:10 +0100

    Fix setDestroyCallback widget argument name

--- janot-jvm/src/main/java/pl/pitcer/janot/gtk/NativeWidget.java
+++ janot-jvm/src/main/java/pl/pitcer/janot/gtk/NativeWidget.java
@@ -34,5 +34,5 @@ final class NativeWidget {
 
 	static native void showAll(ByteBuffer widget);
 
-	static native long setDestroyCallback(ByteBuffer object, DestroyCallback callback);
+	static native long setDestroyCallback(ByteBuffer widget, DestroyCallback callback);
 }
